573d7212c6712d5b9c5bac812449e4df085ca348
Alvarezma <Derpachu@comcast.net>
2018-08-31 08:24:52 -0600

    Ch17 EX1

---
+++ Part4Exercises/Chapter17/MicroTime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MicroTime {
+
+    public static void main ( String[] args ) {
+
+        Scanner scan = new Scanner( System.in );
+
+        int time, items;
+        int min, sec;
+
+        System.out.println("How many items to heat: ");
+        items = scan.nextDouble();
+
+        System.out.println("Enter Time for one item: ");
+        time = scan.nextDouble();
+
+        if (time <= 100) {
+            sec = 100
+        }
+    }
+}
