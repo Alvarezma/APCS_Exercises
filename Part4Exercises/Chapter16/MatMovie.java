e19fda0df8cfdedb5f8aacda32bf2f975ab761a3
Alvarezma <Derpachu@comcast.net>
2018-08-31 07:50:10 -0600

    Initial Ex8

---
+++ Part4Exercises/Chapter16/MatMovie.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class MatMovie {
+
+    public static void main ( String[] args ) {
+
+        Scanner scan = new Scanner( System.in );
+
+        int age, time;
+
+        System.out.println("Enter Age: ");
+        age = scan.nextDouble();
+
+        System.out.println("Enter Time(24 hour): ");
+        temp = scan.nextDouble();
+
+        
+    }
+}
