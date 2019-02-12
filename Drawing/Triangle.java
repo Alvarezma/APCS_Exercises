4fb35d86d85868b6227933f4cab5facf6e8a9ebd
Alvarezma <Derpachu@comcast.net>
2019-02-12 08:28:09 -0700

    TriFractal

--- Drawing/Triangle.java
+++ Drawing/Triangle.java
@@ -2,18 +2,28 @@ import java.awt.geom.Point2D;
 import java.awt.Color;
 
 public class Triangle {
-    private double[] x;
-    private double[] y;
+    private double x1;
+    private double x2;
+    private double x3;
+    private double y1;
+    private double y2;
+    private double y3;
     private Color fill;
 
-    public Triangle(double[] x, double[] y, Color fill)    {
-        this.x = x;
-        this.y = y;
+    public Triangle(double x1, double x2, double x3, double y1, double y2, double y3, Color fill)    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.y3 = y3;
         this.fill = fill;
     }
 
     public void draw()  {
         StdDraw.setPenColor(fill);
+        double[] x = {x1, x2, x3};
+        double[] y = {y1, y2, y3};
         StdDraw.filledPolygon(x, y);
     }
 
---
+++ Drawing/TriangleFactorial.java
@@ -0,0 +1,40 @@
+import java.awt.geom.Point2D;
+import java.awt.Color;
+
+public class TriangleFactorial {
+    private static Triangle t;
+
+    public static void main(String[] args)  {
+        setup();
+
+        fractal(8, 25, 50, 75, 50, 0, 50);
+    }
+
+    public static void setup()  {
+
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(0, 100);
+        StdDraw.enableDoubleBuffering();
+        StdDraw.clear(StdDraw.GRAY);
+        t = new Triangle(0, 50, 100, 0, 100, 0, StdDraw.WHITE);
+        t.draw();
+        StdDraw.show();
+    }
+
+    public static void fractal(int times, double x1, double x2, double x3, double y1, double y2, double y3)   {
+        if (times <= 0) {
+            return;
+        }
+
+        t = new Triangle(x1, x2, x3, y1, y2, y3, StdDraw.BLACK);
+        t.draw();
+
+        fractal(times - 1, x1 - ((x2 - x1)/2), x1, x1 + ((x2 - x1)/2), y2 + ((y1 - y2)/2), y2, y2 + ((y1 - y2)/2));
+        fractal(times - 1, x1 + ((x2 - x1)/2), x2, x3 - ((x2 - x1)/2), y1 + ((y1 - y2)/2), y1, y1 + ((y1 - y2)/2));
+        fractal(times - 1, x3 - ((x2 - x1)/2), x3, x3 + ((x2 - x1)/2), y2 + ((y1 - y2)/2), y2, y2 + ((y1 - y2)/2));
+        StdDraw.show();
+
+    }
+
+    
+}
