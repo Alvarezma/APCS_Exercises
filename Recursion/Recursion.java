1f0e3b7fe7c26d54f7e947323a2e677d2d0d9670
Alvarezma <Derpachu@comcast.net>
2019-02-19 08:28:18 -0700

    merge

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,12 +1,14 @@
 class Recursion    {
 
+
     public static void main(String[] args)  {
     
         System.out.println(factorial(3));
         System.out.println(fibonacci(5));
         System.out.println(reverse("hello"));
         System.out.println(padjenacci(4));
-        
+        int[] list = {0, 3, 4, 6, 8, 15, 20};
+        System.out.println(binarySearch(list, 0));
     }
 
     public static int factorial(int n)  {
@@ -35,5 +37,33 @@ class Recursion    {
             return n;
         }
         return (int)(Math.pow(padjenacci(n-1), 2) - Math.pow(padjenacci(n-2), 2) + Math.pow(padjenacci(n-3), 2));
+    }
+
+    public static boolean binarySearch(int[] list, int target)  {
+        return binarySearch(list, target, 0, list.length - 1);
+    }
+
+    private static boolean binarySearch(int[] list, int target, int min, int max)  {
+        if (max - min <= 1) {
+            return false;
+        }
+        int place = (max - min)/2 + min;
+        if (list[place] == target || list[min] == target || list[max] == target)    {
+            return true;
+        } else if (list[place] < target)  {
+            return binarySearch(list, target, place, max);
+        } else  {
+            return binarySearch(list, target, min, place);
+        }
+    }
+
+    public static String merge(String a, String b) {
+        String answer = "";
+        int place_a = 0;
+        int place_b = 0;
+
+        while (a.length() - 1 != place_a && b.length() - 1 != place_b)  {
+
+        }
     }
 }
